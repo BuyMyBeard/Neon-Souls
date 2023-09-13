4ffee174df1de6733c71cfc4061843a6e3e98c4c
BuyMyBeard <95039323+BuyMyBeard@users.noreply.github.com>
2023-09-13 11:31:37 -0400

    added inputActions

---
+++ Assets/Scripts/Manager/PlayerInputs.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public static class PlayerInputs
+{
+    static readonly int buffer = 100;
+    static readonly ActionMap inputs;
+    static readonly InputAction runAction, jumpAction, interactAction;
+    public static Vector2 LookDelta
+    {
+        get => inputs.PlayerControls.Look.ReadValue<Vector2>();
+    }
+
+    public static bool RunInput { get; private set; } = false;
+    public static bool JumpPress { get; private set; } = false;
+    public static bool JumpHold { get; private set; } = false;
+    public static bool InteractPress { get; private set; } = false;
+    public static bool InteractHold { get; private set; } = false;
+    static PlayerInputs()
+    {
+        inputs = new ActionMap();
+        // runAction = inputs.FindAction("Run");
+        // jumpAction = inputs.FindAction("Jump");
+        // interactAction = inputs.FindAction("Interact");
+
+        inputs.Enable();
+        //runAction.started += (_) => RunInput = true;
+        //runAction.canceled += (_) => RunInput = false;
+        //jumpAction.started += (_) => { JumpHold = true; BufferJump(); };
+        //jumpAction.canceled += (_) => JumpHold = false;
+        //interactAction.started += (_) => { InteractHold = true; BufferInteract(); };
+        //interactAction.canceled += (_) => InteractHold = false;
+    }
+
+    private static void BufferJump()
+    {
+        JumpPress = true;
+        Wait(buffer).ContinueWith(_ => JumpPress = false);
+    }
+    private static void BufferInteract()
+    {
+        InteractPress = true;
+        Wait(buffer).ContinueWith(_ => InteractPress = false);
+    }
+
+    public static async Task Wait(int milliseconds)
+    {
+        await Task.Run(() =>
+        {
+            Task.Delay(milliseconds).Wait();
+        });
+    }
+}
