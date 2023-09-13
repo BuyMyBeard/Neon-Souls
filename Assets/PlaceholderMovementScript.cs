63272ae49d3d937e1b47f5d2f29d8884f054f832
BuyMyBeard <95039323+BuyMyBeard@users.noreply.github.com>
2023-09-13 16:07:08 -0400

    camera 3rd person

--- Assets/PlaceholderMovementScript.cs
+++ Assets/PlaceholderMovementScript.cs
@@ -3,15 +3,28 @@ using UnityEngine;
 public class PlaceholderMovementScript : MonoBehaviour
 {
     [SerializeField] Transform followTarget;
+    [SerializeField] float camMinClamp = -90;
+    [SerializeField] float camMaxClamp = 90;
+    [SerializeField] float lerpSpeed = .5f;
     public float mouseSensitivity = 1f;
-    
+    Quaternion rotationTarget;
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(PlayerInputs.LookDelta.ToString());
-        Vector3 nextRotation = new (-PlayerInputs.LookDelta.y * mouseSensitivity, -PlayerInputs.LookDelta.x * mouseSensitivity, 0);
-        //followTarget.transform.Rotate(nextRotation);
-        followTarget.transform.rotation *= Quaternion.AngleAxis(-PlayerInputs.LookDelta.x , Vector3.up);
+        Quaternion cameraRotation = followTarget.transform.rotation *= Quaternion.AngleAxis(-PlayerInputs.LookDelta.x , Vector3.up);
+        cameraRotation *= Quaternion.AngleAxis(-PlayerInputs.LookDelta.y, Vector3.right);
+
+        Vector3 cameraAngles = cameraRotation.eulerAngles;
+        cameraAngles.z = 0;
+
+        cameraAngles.x = cameraAngles.x > 180 ? cameraAngles.x - 360 : cameraAngles.x;
+        cameraAngles.x = Mathf.Clamp(cameraAngles.x, camMinClamp, camMaxClamp);
+
+        followTarget.transform.localEulerAngles = new Vector3(cameraAngles.x, cameraAngles.y, 0);
+
+        // Quaternion y = Quaternion.Euler(angles.x, angles.y, 0);
+        // rotationTarget = y;
+        // followTarget.transform.rotation = Quaternion.Lerp(transform.rotation, y, Time.deltaTime * lerpSpeed);
     }
 }
