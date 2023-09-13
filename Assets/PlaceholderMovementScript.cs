3704faa635c9d0f5485dc239fe5356a8276e3d5d
Dzubrul <fred.dubrule@gmail.com>
2023-09-13 13:21:45 -0400

    ca avance

--- Assets/PlaceholderMovementScript.cs
+++ Assets/PlaceholderMovementScript.cs
@@ -2,11 +2,16 @@ using UnityEngine;
 
 public class PlaceholderMovementScript : MonoBehaviour
 {
-    [SerializeField] Transform player;
+    [SerializeField] Transform followTarget;
+    public float mouseSensitivity = 1f;
+    Quaternion nextRotation;
+    
 
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log(PlayerInputs.LookDelta.ToString());
+        Vector3 nextRotation = new Vector3(PlayerInputs.LookDelta.y * mouseSensitivity, -PlayerInputs.LookDelta.x * mouseSensitivity, 0);
+        //followTarget.transform.
     }
 }
