6bb0a482fd996dec72817a5ba897be2b9c72a472
Dzubrul <fred.dubrule@gmail.com>
2023-09-13 11:45:12 -0400

    scene de camera

---
+++ Assets/PlaceholderMovementScript.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class PlaceholderMovementScript : MonoBehaviour
+{
+    [SerializeField] Transform player;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
