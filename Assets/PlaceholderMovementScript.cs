c0c71d7300f6300e37c8f5a72e053a41a6ae5bee
BuyMyBeard <95039323+BuyMyBeard@users.noreply.github.com>
2023-09-13 19:21:25 -0400

    added comments to explain behavior

--- Assets/PlaceholderMovementScript.cs
+++ Assets/PlaceholderMovementScript.cs
@@ -9,15 +9,17 @@ public class PlaceholderMovementScript : MonoBehaviour
     public float mouseSensitivity = 1f;
     Quaternion rotationTarget;
 
-    // Update is called once per frame
     void Update()
     {
+        // Quaternion * Quaternion is the same as applying rotation from second to first
         Quaternion cameraRotation = followTarget.transform.rotation *= Quaternion.AngleAxis(PlayerInputs.LookDelta.x , Vector3.up);
         cameraRotation *= Quaternion.AngleAxis(-PlayerInputs.LookDelta.y, Vector3.right);
 
         Vector3 cameraAngles = cameraRotation.eulerAngles;
         cameraAngles.z = 0;
 
+        // convert [0,360[ degrees to ]-180:180] degrees to avoid looping at 0 and allowing negative angles.
+        // 0 degrees is direction of original angle when camera looks at player
         cameraAngles.x = cameraAngles.x > 180 ? cameraAngles.x - 360 : cameraAngles.x;
         cameraAngles.x = Mathf.Clamp(cameraAngles.x, camMinClamp, camMaxClamp);
 
