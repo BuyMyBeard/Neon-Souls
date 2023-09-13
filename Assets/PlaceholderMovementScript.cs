945c7885fb3830d1aab40cd5be77ff878178657f
Dzubrul <fred.dubrule@gmail.com>
2023-09-13 13:49:42 -0400

    lerping time

--- Assets/PlaceholderMovementScript.cs
+++ Assets/PlaceholderMovementScript.cs
@@ -4,14 +4,13 @@ public class PlaceholderMovementScript : MonoBehaviour
 {
     [SerializeField] Transform followTarget;
     public float mouseSensitivity = 1f;
-    Quaternion nextRotation;
     
 
     // Update is called once per frame
     void Update()
     {
         Debug.Log(PlayerInputs.LookDelta.ToString());
-        Vector3 nextRotation = new Vector3(PlayerInputs.LookDelta.y * mouseSensitivity, -PlayerInputs.LookDelta.x * mouseSensitivity, 0);
-        //followTarget.transform.
+        Vector3 nextRotation = new (-PlayerInputs.LookDelta.y * mouseSensitivity, -PlayerInputs.LookDelta.x * mouseSensitivity, 0);
+        followTarget.transform.Rotate(nextRotation);
     }
 }
