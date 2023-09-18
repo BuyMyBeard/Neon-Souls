c628309e3f71f9fe39b469c0477ff5f1e8f69038
BuyMyBeard <95039323+BuyMyBeard@users.noreply.github.com>
2023-09-18 19:18:09 -0400

    Set up linger system for healthbars
    Have yet to test anything. it would be wise to set up Tests for this

--- Assets/Scripts/UI/DisplayBar.cs
+++ Assets/Scripts/UI/DisplayBar.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,23 +9,30 @@ public class DisplayBar : MonoBehaviour
 {
     protected Slider trueHealth, displayedHealth;
     [SerializeField] protected TextMeshProUGUI damageValue;
+    [SerializeField] float lingerTime = 1.5f;
+    [SerializeField] float catchUpSpeed = 1;
+    float timeSinceLastUpdated = 0;
+    bool lingerTimerStarted = false;
+    bool isCatchingUp = false;
+    IEnumerator lingerTimerCoroutine;
+    IEnumerator catchUpCoroutine;
 
-    public float TrueValue
+    protected float TrueValue
     {
         get => trueHealth.value;
         set => trueHealth.value = value;
     }
-    public float DisplayedValue
+    protected float DisplayedValue
     {
         get => displayedHealth.value;
         set => displayedHealth.value = value;
     }
-    public string DamageValue
+    protected string DamageValue
     {
         get => damageValue.text;
         set => damageValue.SetText(value);
     }
-    public bool DisplayDamageValue
+    protected bool DisplayDamageValue
     {
         get => damageValue.gameObject.activeSelf;
         set => damageValue.gameObject.SetActive(value);
@@ -33,5 +41,58 @@ public class DisplayBar : MonoBehaviour
     {   
         displayedHealth = GetComponent<Slider>();
         trueHealth = GetComponentInChildren<Slider>();
+
+    }
+    private void Update()
+    {
+        timeSinceLastUpdated += Time.deltaTime;
+    }
+
+    // TODO: Have yet to test any of this
+    public void Add(float value, float max)
+    {
+        TrueValue += value / max;
+        if (lingerTimerStarted)
+            StopCoroutine(lingerTimerCoroutine);
+        if (isCatchingUp)
+            StartCoroutine(UpdateWhenDoneCatchingUp());
+    }
+    public void Remove(float value, float max)
+    {
+        TrueValue -= value / max;
+        lingerTimerCoroutine = LingerTimer();
+        if (!lingerTimerStarted)
+            StartCoroutine(lingerTimerCoroutine);
+        timeSinceLastUpdated = 0;
+        DisplayDamageValue = true;
+        DamageValue = value.ToString();
+    }
+
+    IEnumerator LingerTimer()
+    {
+        lingerTimerStarted = true;
+        yield return new WaitUntil(() => timeSinceLastUpdated > lingerTime);
+        catchUpCoroutine = CatchUp(TrueValue);
+        StartCoroutine(catchUpCoroutine);
+        lingerTimerStarted = false;
+        DisplayDamageValue = false;
+    }
+
+    IEnumerator CatchUp(float goal)
+    {
+        isCatchingUp = true;
+        float start = DisplayedValue;
+        for (float t = 0; t <= 1; t += Time.deltaTime * catchUpSpeed)
+        {
+            yield return null;
+            DisplayedValue = Mathf.Lerp(start, goal, t);
+        }
+        DisplayedValue = goal;
+        isCatchingUp = false;
+    }
+    IEnumerator UpdateWhenDoneCatchingUp()
+    {
+        yield return new WaitUntil(() => !isCatchingUp);
+        DisplayedValue = TrueValue;
     }
 }
