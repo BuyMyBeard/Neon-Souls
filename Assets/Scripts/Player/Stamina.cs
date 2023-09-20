ed5048f1af6bb23941444ad75bc650ca2cb3a54f
Dzubrul <fred.dubrule@gmail.com>
2023-09-20 12:11:24 -0400

    debut stamina

--- Assets/Scripts/Player/Stamina.cs
+++ Assets/Scripts/Player/Stamina.cs
@@ -7,10 +7,18 @@ public class Stamina : MonoBehaviour
 {
     DisplayBar playerStaminabar;
     [SerializeField] int maxStamina = 100;
-    [SerializeField] int currentStamina;
+    [SerializeField] float currentStamina;
+    [SerializeField] float staminaRegenRate = 10000f;
     [SerializeField] int dodgeStamina = 20;
     [SerializeField] int lightAttackStamina = 25;
     [SerializeField] int heavyAttackStamina = 40;
+    [SerializeField] float exhaustionTime = 0.5f;
+    float exhaustionTimer = 0;
+    Coroutine exhaustionTimerCoroutine;
+    Coroutine regenStaminaCoroutine;
+    bool exhaustionTimerStarted = false;
+    public bool IsExhausted { get => currentStamina <= 0; }
+
     private void Awake()
     {
         playerStaminabar = GameObject.FindGameObjectWithTag("DisplayedStamina").GetComponent<DisplayBar>();
@@ -19,13 +27,23 @@ public class Stamina : MonoBehaviour
     {
         ResetStamina();
     }
-    private void OnDodge()
+    private void Update()
     {
-        if(currentStamina > 0)
+        exhaustionTimer += Time.deltaTime;
+    }
+    private void OnDodge()//Maybe change to receive the stamina consumption from another component.
+    {
+        if(!IsExhausted)
         {
             currentStamina -= dodgeStamina;
             playerStaminabar.Remove(dodgeStamina, maxStamina, true);
+            if (!exhaustionTimerStarted)
+                exhaustionTimerCoroutine = StartCoroutine(ExhaustionTimer());
+            else
+                StopCoroutine(exhaustionTimerCoroutine);
+
         }
+
     }
     private void OnLightAttack()
     {
@@ -38,6 +56,28 @@ public class Stamina : MonoBehaviour
     private void ResetStamina()
     {
         currentStamina = maxStamina;
+    }
+    private IEnumerator ExhaustionTimer()
+    {
+        exhaustionTimerStarted = true;
+        yield return new WaitUntil(() => exhaustionTimer > exhaustionTime);
+        regenStaminaCoroutine = StartCoroutine(RegenStamina());
+        
+
+    }
+    private IEnumerator RegenStamina()
+    {  
+        exhaustionTimer = 0;
+
+        while(currentStamina < maxStamina)
+        {
+            yield return null;
+            float staminaToRegen = (staminaRegenRate * Time.deltaTime);
+            currentStamina += staminaToRegen;
+            playerStaminabar.Add((int)staminaToRegen, maxStamina);     
+        }
+        ResetStamina();
+        exhaustionTimerStarted = false;
     }
 
 }
