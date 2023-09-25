1335a3d1a0fe9c825db405eaed6973e22e56920e
BuyMyBeard <95039323+BuyMyBeard@users.noreply.github.com>
2023-09-25 11:38:59 -0400

    Refactoring

--- Assets/Scripts/Environment/Bonfire.cs
+++ Assets/Scripts/Environment/Bonfire.cs
@@ -2,29 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BoneFireInteractble : Interactable
+public class Bonfire : Interactable
 {
-    BonfirerManager bonefirerManager;
+    BonfireManager bonfireManager;
     public bool active = false;
+    [SerializeField] Vector3 respawnOffset = Vector3.zero;
 
+    public Vector3 RespawnOffset { get => respawnOffset; }
     protected override void Awake()
     {
         base.Awake();
-        bonefirerManager = FindObjectOfType<BonfirerManager>();
-        this.promptMessage = "Ligth Fire";
+        bonfireManager = FindObjectOfType<BonfireManager>();
+        promptMessage = "Light Fire";
     }
     public override void Interact()
     {
         if (active) 
         {
-            bonefirerManager.SitAtBonFire(this);
+            bonfireManager.SitAtBonfire(this);
         }
         else
         {
             active = true;
-            bonefirerManager.ActivateBonfire(this);
+            bonfireManager.ActivateBonfire(this);
             StartCoroutine(FlickerCollider());
-            this.promptMessage = "Sit";
+            promptMessage = "Sit";
         }
     }
     IEnumerator FlickerCollider()
