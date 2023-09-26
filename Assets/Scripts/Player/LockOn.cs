ea47a4636a859f458cfa5655ed037531aeb36153
Dzubrul <fred.dubrule@gmail.com>
2023-09-25 22:09:08 -0400

    overlapshere et raycast fonctionnement pas...

--- Assets/Scripts/Player/LockOn.cs
+++ Assets/Scripts/Player/LockOn.cs
@@ -5,17 +5,22 @@ using UnityEngine;
 public class LockOn : MonoBehaviour
 {
     bool isLocked = false;
-    List<Enemy> enemiesInSight = new ();
+    [SerializeField] float viewRadius = 90;
+    [SerializeField, Range(0,360)] float viewAngle = 90;
+    [SerializeField] LayerMask lockableMask;
+    [SerializeField] LayerMask environmentMask;
+    List<Lockable> enemiesInSight = new ();
+
+    
     Transform player;
     Transform camFollowTarget;
-    [SerializeField] GameObject capsule;
-    public void AddToLockOnList(Enemy enemy)
+    public void AddToLockOnList(Lockable lockable)
     {
-        enemiesInSight.Add(enemy); 
+        enemiesInSight.Add(lockable); 
     }
-    public void RemoveFromLockOnList(Enemy enemy)
+    public void RemoveFromLockOnList(Lockable lockable)
     {
-        enemiesInSight.Remove(enemy);
+        enemiesInSight.Remove(lockable);
     }
     public void Awake()
     {
@@ -28,7 +33,7 @@ public class LockOn : MonoBehaviour
         {
             return;
         }
-
+        FindEnemiesInSight();
         
     }
     void OnLockOn()
@@ -40,7 +45,8 @@ public class LockOn : MonoBehaviour
         }
         else if (enemiesInSight.Count > 0)
         {
-            Debug.Log("enemies is more than 0");
+
+            Debug.Log("enemies are more than 0");
         }
         else
         {
@@ -50,6 +56,26 @@ public class LockOn : MonoBehaviour
         
         
     }
+    void FindEnemiesInSight()
+    {
+        enemiesInSight.Clear();
+        Collider[] enemiesInViewRadius = Physics.OverlapSphere(camFollowTarget.position, viewRadius, lockableMask);
+        for(int i = 0; i< enemiesInViewRadius.Length; i++)
+        {
+            Transform enemy = enemiesInSight[i].transform;
+            Vector3 directionToEnemy = (enemy.position - camFollowTarget.position).normalized;
+            if(Vector3.Angle(camFollowTarget.forward, directionToEnemy) < viewAngle / 2)
+            {
+                float distanceToTarget = Vector3.Distance(camFollowTarget.position, enemy.position);
+                if(!Physics.Raycast(camFollowTarget.position, directionToEnemy, distanceToTarget, environmentMask))
+                {
+                    enemiesInSight.Add(enemy.GetComponent<Lockable>());
+                    Debug.Log(enemy.name + "Added to list");
+                }
+            }
+        }
+    }
+
     /// <summary>
     /// Lerp fonctionne pas mais la camera snap au bon endroit. So, it kinda works.
     /// </summary>
@@ -64,5 +90,13 @@ public class LockOn : MonoBehaviour
             elapsedTime += Time.deltaTime;
             yield return null;
         }
+    }
+    public Vector3 DirectionFromAngle(float angleInDegrees, bool angleIsGlobal)
+    {
+        if (!angleIsGlobal)
+        {
+            angleInDegrees += camFollowTarget.transform.eulerAngles.y;
+        }
+        return new Vector3(Mathf.Sin(angleInDegrees * Mathf.Deg2Rad), 0, Mathf.Cos(angleInDegrees * Mathf.Deg2Rad));
     }
 }
