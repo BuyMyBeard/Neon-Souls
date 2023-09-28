5acd661beea700ba1cec4a146dd32ffed57e0993
JeremyDeschamps <Deschampsj007@gmail.com>
2023-09-27 21:53:55 -0400

    regler potion bugger

--- Assets/Scripts/Player/Potions.cs
+++ Assets/Scripts/Player/Potions.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Potions : MonoBehaviour,IRechargeable
+public class Potions : MonoBehaviour ,IRechargeable
 {
 
     [SerializeField] Material potionMat;
