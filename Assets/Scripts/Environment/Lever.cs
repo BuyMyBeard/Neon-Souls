5c7caad13eeeeb95e2a82e86c49ba5110d952a9d
BuyMyBeard <95039323+BuyMyBeard@users.noreply.github.com>
2023-09-29 00:37:47 -0400

    refactoring

--- Assets/Scripts/Environment/Lever.cs
+++ Assets/Scripts/Environment/Lever.cs
@@ -3,21 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Lever : Interactable
-{
-    
+{   
     public override void Interact()
     {
         base.Interact();
         Debug.Log("Levier IS PRESSED");
-    }
-
-    protected override void OnTriggerEnter(Collider other)
-    {
-        base.OnTriggerEnter(other);
-    }
-
-    protected override void OnTriggerExit(Collider other)
-    {
-        base.OnTriggerExit(other);
     }
 }
