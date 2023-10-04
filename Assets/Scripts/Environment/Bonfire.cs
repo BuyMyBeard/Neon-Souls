888315e253d61404b774961a485700e9e901cce1
BuyMyBeard <95039323+BuyMyBeard@users.noreply.github.com>
2023-10-04 16:00:06 -0400

    rl`frklfr

--- Assets/Scripts/Environment/Bonfire.cs
+++ Assets/Scripts/Environment/Bonfire.cs
@@ -7,13 +7,14 @@ public class Bonfire : Interactable
     BonfireManager bonfireManager;
     public bool active = false;
     [SerializeField] Vector3 respawnOffset = Vector3.zero;
+    new Light light;
 
     public Vector3 RespawnOffset { get => respawnOffset; }
     protected override void Awake()
     {
         base.Awake();
         bonfireManager = FindObjectOfType<BonfireManager>();
-        promptMessage = "Light Fire";
+        light = GetComponent<Light>();
     }
     public override void Interact()
     {
@@ -22,10 +23,11 @@ public class Bonfire : Interactable
             bonfireManager.SitAtBonfire(this);
             return;
         }
+        light.color = new Color(0, 1, 0);
         active = true;
         bonfireManager.ActivateBonfire(this);
         StartCoroutine(FlickerCollider());
-        promptMessage = "Sit";
+        promptMessage = "Play";
     }
     IEnumerator FlickerCollider()
     {
--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -11,7 +11,7 @@ using System.Collections;using System.Collections.Generic;using System.Linq;u
         health = player.GetComponentInParent<Health>();
         playerCharacter = player.GetComponentInParent<CharacterController>();
         playerMovement = player.GetComponentInParent<PlayerMovement>();
-        cameraMovement = player.GetComponentInParent<CameraMovement>();    }    public void PlayerDie()    {        bonfireManager.Respawn();        RechargeEverything();    }    public void RechargeEverything()    {        // TODO: Enemy Spawns        foreach (IRechargeable rechargeable in rechargeables)        {            rechargeable.Recharge();        }    }
+        cameraMovement = player.GetComponentInParent<CameraMovement>();    }    public void PlayerDie()    {        bonfireManager.Respawn();        RechargeEverything();    }    public void RechargeEverything()    {        // TODO: Enemy Spawns , Piege Recharge        foreach (IRechargeable rechargeable in rechargeables)        {            rechargeable.Recharge();        }    }
     public void StartIFrame() => health.invincible = true;
     public void StopIFrame() => health.invincible = false;
     public void FreezePlayer() { playerCharacter.enabled = false; playerMovement.frozen = true; }
