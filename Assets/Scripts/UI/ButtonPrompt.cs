8e3114ede6d78023a7f39e2275b6fee3422de716
JeremyDeschamps <Deschampsj007@gmail.com>
2023-10-04 10:00:28 -0400

    last refactoring

--- Assets/Scripts/UI/ButtonPrompt.cs
+++ Assets/Scripts/UI/ButtonPrompt.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using System.Linq;
 using TMPro;
 using UnityEngine;
-using UnityEngine.Assertions.Must;
 using UnityEngine.InputSystem;
 
 [RequireComponent(typeof(Collider))]
