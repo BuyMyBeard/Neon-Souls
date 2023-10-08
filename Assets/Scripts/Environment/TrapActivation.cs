68870ac33621edf8c41e3e3f2169e6e62a2a8a47
Dzubrul <fred.dubrule@gmail.com>
2023-10-08 13:26:22 -0400

    pressure plate fixed

--- Assets/Scripts/Environment/TrapActivation.cs
+++ Assets/Scripts/Environment/TrapActivation.cs
@@ -6,17 +6,40 @@ public class TrapActivation : MonoBehaviour
 {
     //TO DO : doit recharger avec la mort ou un siting 
     public bool wasTriggered = false;
+    Transform physicalSwitch;
+    Vector3 physicalSwitchInitalPosition;
+    private void Start()
+    {
+        physicalSwitch = transform.parent.GetChild(1);
+        Debug.Log(physicalSwitch.name);
+        physicalSwitchInitalPosition = physicalSwitch.position;
+    }
     void OnTriggerEnter(Collider c)
     {
         if(wasTriggered) return;
 
         gameObject.GetComponent<Trap>().Trigger();
         gameObject.GetComponent<Collider>().enabled = false;
+        StartCoroutine(LerpPosition());
         wasTriggered = true;
     }
     public void ResetTrap()
     {
         gameObject.GetComponent<Collider>().enabled = true;
+        physicalSwitch.position = physicalSwitchInitalPosition;
         wasTriggered = false;
+    }
+    IEnumerator LerpPosition()
+    {
+        float timeElapsed = 0;
+        float lerpDuration = 3;
+        Vector3 endPosition = new Vector3(physicalSwitchInitalPosition.x, physicalSwitchInitalPosition.y - 0.5f, physicalSwitchInitalPosition.z);
+        while(timeElapsed < lerpDuration)
+        {
+            physicalSwitch.position = Vector3.Lerp(physicalSwitchInitalPosition, endPosition, timeElapsed / lerpDuration);
+            timeElapsed += Time.deltaTime;
+            yield return null;
+        }
+        physicalSwitch.position = endPosition;
     }
 }
