86cc4b82e2da3062674ed830bff7a433e37a10e1
Dzubrul <fred.dubrule@gmail.com>
2023-10-23 21:34:44 -0400

    Stagger in progress, animations mapped

--- Assets/Scripts/Character/Player/PlayerHealth.cs
+++ Assets/Scripts/Character/Player/PlayerHealth.cs
@@ -5,32 +5,36 @@ using UnityEngine;
 public class PlayerHealth : Health
 {
     Stamina stamina;
+    Stagger stagger;
     Block block;
     
     private new void Awake()
     {
         base.Awake();
         stamina = GetComponent<Stamina>();
+        stagger = GetComponent<Stagger>();
         block = GetComponent<Block>();
     }
     public void InflictUnblockableDamage(int damage) => InflictDamage(damage);
     public void InflictBlockableDamage(int damage, int staminaBlockCost, Transform attackerPosition)
-    {  
+    {
         if (block.IsParrying && !stamina.IsExhausted && IsAttackerInFront(attackerPosition))
         {
             stamina.Remove(staminaBlockCost);
             block.ResetParryWindow();
+            stagger.BlockHit();
         }
         else if (block.IsBlocking && !stamina.IsExhausted && IsAttackerInFront(attackerPosition)) 
         {
             int damageReduced = (int) (damage * block.DamageReduction);
             stamina.Remove(staminaBlockCost);
-            base.InflictDamage(damageReduced);
+            InflictDamage(damageReduced);
+            stagger.BlockHit();
         }
         else
         {
-            // Staggered
             InflictDamage(damage);
+            stagger.BecomeStaggered(attackerPosition);
         }
     }
     bool IsAttackerInFront(Transform attackerPosition)
