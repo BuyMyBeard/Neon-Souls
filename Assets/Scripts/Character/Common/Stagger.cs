32ae4204210194580bc35fae10be8bfba868c2f2
Dzubrul <fred.dubrule@gmail.com>
2023-10-23 21:34:44 -0400

    Stagger in progress, animations mapped

--- Assets/Scripts/Character/Common/Stagger.cs
+++ Assets/Scripts/Character/Common/Stagger.cs
@@ -6,28 +6,37 @@ public class Stagger : MonoBehaviour
 {
     Animator animator;
     PlayerAnimationEvents animationEvents;
+    Health health;
     public bool IsStaggered { get; private set; } = false;
 
     private void Awake()
     {
         animator = GetComponentInChildren<Animator>();
         animationEvents = GetComponentInChildren<PlayerAnimationEvents>();
+        health = GetComponent<Health>();
     }
-    public void Update()
+    public void BecomeStaggered(Transform target)
     {
-        //if (!IsStaggered)
-        //{
-        //    animator.SetBool("IsStagggered", true);
-        //    animationEvents.ReduceMovement();
-        //    IsStaggered = true;
-        //}
-        //else
-        //{
-        //    animator.SetBool("IsStagggered", false);
-        //    animationEvents.RestoreMovement();
-        //    IsStaggered = false;
-        //}
+        if (health.IsDead)
+            return;
+        animationEvents.FreezeRotation();
+        animationEvents.FreezeMovement();
+        animationEvents.DisableActions();
+        Vector3 playerPlanePos = new Vector3(animator.transform.position.x,0,animator.transform.position.z);
+        Vector3 targetPlanePos = new Vector3(target.transform.position.x, 0, target.transform.position.z);
+        Vector3 targetDir = animator.transform.InverseTransformDirection((targetPlanePos - playerPlanePos).normalized);
+
+        animator.SetTrigger("Staggered");
+        animator.SetFloat("StaggerX", targetDir.x);
+        animator.SetFloat("StaggerY", targetDir.z);
     }
-
-
+    public void BlockHit()
+    {
+        if (health.IsDead)
+            return;
+        animationEvents.FreezeRotation();
+        animationEvents.FreezeMovement();
+        animationEvents.DisableActions();
+        animator.SetTrigger("BlockHit");
+    }
 }
