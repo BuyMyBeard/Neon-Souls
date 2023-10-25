374f9d63657cc01306550cc42cbd04402b3c82aa
JeremyDeschamps <Deschampsj007@gmail.com>
2023-10-25 17:31:50 -0400

    Faut continuer avec LE UI Pour actver le cotroler dessus (ALEX NEED HELP)

--- Assets/XpMenuManager.cs
+++ Assets/XpMenuManager.cs
@@ -8,20 +8,34 @@ using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class XpMenuManager : MonoBehaviour
-{
+{a
     Canvas menuDisplay;
+    PlayerController playerController;
+    ButtonXp[] buttons;
 
     private void Awake()
     {
         menuDisplay = GetComponentInChildren<Canvas>();
+        buttons = FindObjectsOfType<ButtonXp>();
+        playerController = FindObjectOfType<PlayerController>();
     }
     void Start()
     {
-        menuDisplay.enabled = false; 
+        Hide();
     }
-
+    public void Hide()
+    {
+        playerController.SwitchToPlayerControls();
+        menuDisplay.enabled = false;
+        foreach (ButtonXp button in buttons)
+        {
+            button.ChangeColor(Color.white);
+        }
+        Time.timeScale = 1f;
+    }
     public void Show()
     {
+        playerController.SwitchToUI();
         menuDisplay.enabled = true;
         Time.timeScale = 0f;
     }
