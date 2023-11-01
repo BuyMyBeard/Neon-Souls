fa02c7008eff3ea5916f6d4d06e51c0f28cc4604
Dzubrul <fred.dubrule@gmail.com>
2023-11-01 16:20:14 -0400

    XpDropOnDeath work, integrated with xp menus

--- Assets/XpSystem/PlayerExperience.cs
+++ Assets/XpSystem/PlayerExperience.cs
@@ -7,11 +7,15 @@ using UnityEngine.UI;
 public class PlayerExperience : MonoBehaviour, IXpReceiver
 {
     [SerializeField] string soulsTextTag = "SoulsCount";
+    [SerializeField] string transactionTextTag = "TransactionText";
     [SerializeField]int xpAmount = 0;
+    [SerializeField] Animator transactionAnimator;
+    TextMeshProUGUI transactionText;
     TextMeshProUGUI xpText;
     private void Awake()
     {
         xpText = GameObject.FindGameObjectWithTag(soulsTextTag).GetComponent<TextMeshProUGUI>();
+        transactionText = GameObject.FindGameObjectWithTag(transactionTextTag).GetComponent<TextMeshProUGUI>();
         xpText.text = xpAmount.ToString();
     }
     public int XpAmount => xpAmount; 
@@ -19,12 +23,19 @@ public class PlayerExperience : MonoBehaviour, IXpReceiver
     public void GainXp(int amount)
     { 
         xpAmount += amount;
+        transactionText.text = "+" + amount.ToString();
+        transactionAnimator.Play("SoulsCount");
+    }
+    public void UpdateDisplay()
+    {
         xpText.text = xpAmount.ToString();
     }
 
     public void removeXp(int amount)
     {
         xpAmount -= amount;
-        xpText.text = xpAmount.ToString();
+        Debug.Log(amount);
+        transactionText.text = "-" + amount.ToString();
+        transactionAnimator.Play("SoulsCount");
     }
 }
