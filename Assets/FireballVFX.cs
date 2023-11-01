7fbeb00d51f3b514a981df4110d1170597fe8517
JeremyDeschamps <Deschampsj007@gmail.com>
2023-11-01 14:55:46 -0400

    Fireball Explosion is finished

---
+++ Assets/FireballVFX.cs
@@ -0,0 +1,31 @@
+using Microsoft.Unity.VisualStudio.Editor;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VFX;
+
+[RequireComponent(typeof(VisualEffect))]
+public class FireballVFX : MonoBehaviour
+{
+    VisualEffect fireballVFX;
+    float scale = 0;
+
+    private void Awake()
+    {
+        fireballVFX = GetComponent<VisualEffect>();
+    }
+
+    private void OnEnable()
+    {
+        StartCoroutine(FiraballSpawning());
+    }
+    IEnumerator FiraballSpawning()
+    {
+        while (scale < 1) 
+        {
+            fireballVFX.SetFloat("sizeScale", scale += .1f);
+            yield return new WaitForSeconds(0.1f);
+        }
+
+    }
+}
