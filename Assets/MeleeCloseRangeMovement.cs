3c1618cf3f5597c406df288844d298eef5136cf5
BuyMyBeard <95039323+BuyMyBeard@users.noreply.github.com>
2023-11-06 13:41:12 -0500

    added trystayclose component

--- Assets/MeleeCloseRangeMovement.cs
+++ Assets/MeleeCloseRangeMovement.cs
@@ -1,18 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
-public class MeleeCloseRangeMovement : MonoBehaviour
+public class MeleeTryGetClose : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    Enemy enemy;
+    [SerializeField] float closeIn;
+    [SerializeField] float closeOut;
+    private void Awake()
     {
-        
+        enemy = GetComponent<Enemy>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        enemy.inRangeInitEvent.AddListener(StartTryGetClose);
+        enemy.inRangeExitEvent.AddListener(StopTryGetClose);
+        enemy.closeInitEvent.AddListener(StartCheckIfStillClose);
+        enemy.closeExitEvent.AddListener(StopCheckIfStillClose);
     }
+    private void StartTryGetClose() => StartCoroutine(nameof(TryGetClose));
+    private void StopTryGetClose() => StopCoroutine(nameof(TryGetClose));
+    private void StartCheckIfStillClose() => StartCoroutine(nameof(CheckIfStillClose));
+    private void StopCheckIfStillClose() => StopCoroutine(nameof(CheckIfStillClose));
+    IEnumerator TryGetClose()
+    {
+        yield return new WaitUntil(() => enemy.DistanceFromPlayer.magnitude < closeIn);
+        enemy.ChangeMode(Enemy.ModeId.Close);
+    }
+    IEnumerator CheckIfStillClose()
+    {
+        yield return new WaitUntil(() => enemy.DistanceFromPlayer.magnitude > closeOut);
+        enemy.ChangeMode(Enemy.ModeId.InRange);
+    }
+    
+
 }
