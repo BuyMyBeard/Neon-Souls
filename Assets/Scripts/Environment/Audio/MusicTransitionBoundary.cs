4754c8a80109a2fcd5c6765b3b87b3d522a48d0e
JohnP55 <yannickturpin@outlook.com>
2023-11-06 20:20:22 -0500

    something idk

--- Assets/Scripts/Environment/Audio/MusicTransitionBoundary.cs
+++ Assets/Scripts/Environment/Audio/MusicTransitionBoundary.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class MusicTransitionBoundary : MonoBehaviour
 {
     public float lowPassValue;
+    public MusicTransitionManager transitionManager;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        transitionManager = GetComponentInParent<MusicTransitionManager>();
     }
 
     // Update is called once per frame
