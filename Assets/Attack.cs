8169f9453797922607cbd5f9b7c2596dc3bb7d06
BuyMyBeard <95039323+BuyMyBeard@users.noreply.github.com>
2023-11-11 16:53:52 -0500

    attack combos in game

--- Assets/Attack.cs
+++ Assets/Attack.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class Attack : StateMachineBehaviour
 {
+    [SerializeField] bool unFreezeRotation = false;
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         animator.SendMessage("StopStaminaRegen");
         animator.SendMessage("DisableActions");
         animator.SendMessage("FreezeMovement");
         animator.SetBool("Attacking", true);
+        if (unFreezeRotation) animator.SendMessage("UnFreezeRotation");
     }
     //public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     //{
