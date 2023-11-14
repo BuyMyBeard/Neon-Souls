38b06aa0e6a44fe4fc2a22cd8c717375a2cd655b
BuyMyBeard <95039323+BuyMyBeard@users.noreply.github.com>
2023-11-14 15:47:57 -0500

    Update BossManager.cs

--- Assets/Scripts/Character/Boss/BossManager.cs
+++ Assets/Scripts/Character/Boss/BossManager.cs
@@ -2,17 +2,41 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BossManager : MonoBehaviour
+public class BossManager : MonoBehaviour, IRechargeable
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] BossHealth boss1;
+    [SerializeField] BossHealth boss2;
+
+    [SerializeField] DisplayBar bossBar1;
+    [SerializeField] DisplayBar bossBar2;
+
+    PlayerAnimationEvents playerAnimationEvents;
+    bool defeated = false;
+    bool cinematicInProgress = false;
+    void Awake()
     {
-        
+        playerAnimationEvents = FindObjectOfType<PlayerAnimationEvents>();
+    }
+    public void StartBossFight()
+    {
+        if (defeated) return;
+        StartCoroutine(nameof(BossFight));
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator BossFight()
     {
-        
+        cinematicInProgress = true;
+        playerAnimationEvents.DisableActions();
+        playerAnimationEvents.FreezeCamera();
+        yield return new WaitWhile(() => cinematicInProgress);
+        playerAnimationEvents.ResetAll();
+        yield return new WaitWhile(() => boss1.CurrentHealth > 0 && boss2.CurrentHealth > 0);
+        defeated = true;
     }
+    public void Recharge()
+    {
+        if (defeated) return;
+        StopCoroutine(nameof(BossFight));
+    }
+    public void EndCinematic() => cinematicInProgress = false;
 }
