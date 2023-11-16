439bf8718bbbe9a609d76e92247dae4bec9dc23b
BuyMyBeard <95039323+BuyMyBeard@users.noreply.github.com>
2023-11-16 00:28:10 -0500

    cutscene done

--- Assets/Scripts/Character/Boss/BossHealth.cs
+++ Assets/Scripts/Character/Boss/BossHealth.cs
@@ -4,8 +4,27 @@ using UnityEngine;
 
 public class BossHealth : EnemyHealth
 {
+    [SerializeField] float healthbarLingerTime = 2;
     public override void HandleHealthbar(int damage = 0)
     {
-        
+        displayHealthbar.Remove(damage, maxHealth, true);
+    }
+    protected override void Die()
+    {
+        invincible = true;
+        animator.ResetAllTriggers();
+        animator.SetTrigger("Die");
+        animationEvents.DisableActions();
+        animationEvents.FreezeMovement();
+        animationEvents.FreezeRotation();
+        animationEvents.StartIFrame();
+        if (canFallApart) fallApart.DetachWeapons();
+        StartCoroutine(HideBar());
+    }
+
+    IEnumerator HideBar()
+    {
+        yield return new WaitForSeconds(healthbarLingerTime);
+        displayHealthbar.Hide();
     }
 }
