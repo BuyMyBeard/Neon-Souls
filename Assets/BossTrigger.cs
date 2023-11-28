5803675705c9ba4423452da922429e051a39bc63
JohnP55 <yannickturpin@outlook.com>
2023-11-28 18:15:11 -0500

    Fixed boss foggate width, boss trigger not respawning, etc

--- Assets/BossTrigger.cs
+++ Assets/BossTrigger.cs
@@ -1,20 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
-public class BossTrigger : MonoBehaviour
+public class BossTrigger : MonoBehaviour, IRechargeable
 {
-   BossManager bossManager;
+    BossManager bossManager;
     Collider bossTrigger;
+    PlayerAnimationEvents playerAnimationEvents;
+    [SerializeField] float timeBeforeCutsceneStart;
+
     private void Awake()
     {
         bossManager = FindAnyObjectByType<BossManager>();
         bossTrigger = GetComponent<Collider>();
+        playerAnimationEvents = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerAnimationEvents>();
     }
     private void OnTriggerEnter(Collider other)
     {
-        Invoke("StartCutScene", 3.10f);
+        playerAnimationEvents.FreezeMovement();
+        playerAnimationEvents.FreezeRotation();
+        Invoke("StartCutScene", timeBeforeCutsceneStart);
         bossTrigger.enabled = false;
     }
     private void StartCutScene() => bossManager.StartCutscene();
+    public void Recharge(RechargeType rechargeType = RechargeType.Respawn)
+    {
+        bossTrigger.enabled = true;
+    }
 }
